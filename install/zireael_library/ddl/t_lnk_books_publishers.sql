6188904447f6d616e58306201abfeabd0e0c6eef
sfedorov <sfedorov@zuzex.com>
2022-12-10 21:20:19 +0300

    added new table, trigger, sequence for publisher, added way to meta file

--- install/zireael_library/ddl/t_lnk_books_publishers.sql
+++ install/zireael_library/ddl/t_lnk_books_publishers.sql
@@ -4,11 +4,11 @@ create table t_lnk_books_publishers (
 );
 
 alter table t_lnk_books_publishers
-    add constraint lnk_book_fk foreign key(book_id)
+    add constraint lnk_book_publisher_fk foreign key(book_id)
         references t_books(id);
 
 alter table t_lnk_books_publishers
-    add constraint lnk_publisher_fk foreign key(publisher_id)
+    add constraint lnk_publisher_book_fk foreign key(publisher_id)
         references t_publishers(id);
 
 comment on table t_lnk_books_publishers is 'Books and publisher link table';
--- install/zireael_library/zireael_library.sql
+++ install/zireael_library/zireael_library.sql
@@ -121,3 +121,38 @@ comment on table t_lnk_books_publishers is 'Books and publisher link table';
 comment on column t_lnk_books_publishers.book_id is 'Book ID';
 comment on column t_lnk_books_publishers.publisher_id is 'Publisher ID';
 
+--t_lnk_books_authors - Books and author link table
+create table t_lnk_books_authors (
+    book_id number(10) not null,
+    author_id number(10) not null
+);
+
+alter table t_lnk_books_authors
+    add constraint lnk_book_author_fk foreign key(book_id)
+        references t_books(id);
+
+alter table t_lnk_books_authors
+    add constraint lnk_author_book_fk foreign key(author_id)
+        references t_authors(id);
+
+comment on table t_lnk_books_authors is 'Books and author link table';
+comment on column t_lnk_books_authors.book_id is 'Book ID';
+comment on column t_lnk_books_authors.author_id is 'Author ID';
+
+--t_lnk_books_genres - Books and genre link table
+create table t_lnk_books_genres (
+    book_id number(10) not null,
+    genre_id number(10) not null
+);
+
+alter table t_lnk_books_genres
+    add constraint lnk_book_genre_fk foreign key(book_id)
+        references t_books(id);
+
+alter table t_lnk_books_genres
+    add constraint lnk_genre_book_fk foreign key(genre_id)
+        references t_genres(id);
+
+comment on table t_lnk_books_genres is 'Books and genre link table';
+comment on column t_lnk_books_genres.book_id is 'Book ID';
+comment on column t_lnk_books_genres.genre_id is 'Genre ID';
