83c04523a287d43b5bfc01c52f188eefa2a74787
sfedorov <sfedorov@zuzex.com>
2022-12-10 21:11:19 +0300

    added new table, trigger, sequence for publisher, added way to meta file

---
+++ install/zireael_library/ddl/author_seq.sql
@@ -0,0 +1,3 @@
+create sequence author_seq
+start with 1
+increment by 1;
--- install/zireael_library/zireael_library.sql
+++ install/zireael_library/zireael_library.sql
@@ -72,4 +72,52 @@ begin
 end;
 /
 
---
+--t_authors - Author table
+create sequence author_seq
+start with 1
+increment by 1;
+
+create table t_authors (
+    id number(10),
+    first_name varchar2(100) not null,
+    last_name varchar2(100),
+    third_name varchar2(100),
+    constraint author_pk primary key(id)
+);
+
+alter table t_authors
+    add constraint name_uq unique(first_name, last_name, third_name);
+
+comment on table t_authors is 'Author table';
+comment on column t_authors.id is 'Author ID';
+comment on column t_authors.first_name is 'Author''s name';
+comment on column t_authors.last_name is 'Author''s last name';
+comment on column t_authors.third_name is 'Author''s third name';
+
+
+create or replace trigger author_tbi
+    before insert on t_authors
+    for each row
+begin
+    :new.id := genre_seq.nextval;
+end;
+/
+
+--t_lnk_books_publishers - Books and publisher link table
+create table t_lnk_books_publishers (
+    book_id number(10) not null,
+    publisher_id number(10) not null
+);
+
+alter table t_lnk_books_publishers
+    add constraint lnk_book_fk foreign key(book_id)
+        references t_books(id);
+
+alter table t_lnk_books_publishers
+    add constraint lnk_publisher_fk foreign key(publisher_id)
+        references t_publishers(id);
+
+comment on table t_lnk_books_publishers is 'Books and publisher link table';
+comment on column t_lnk_books_publishers.book_id is 'Book ID';
+comment on column t_lnk_books_publishers.publisher_id is 'Publisher ID';
+
